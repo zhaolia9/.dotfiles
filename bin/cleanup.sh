6d648c0d2bc126eeacb8605541e191e4423dc47a
zhaolia9 <111785855+zhaolia9@users.noreply.github.com>
2023-04-03 15:26:53 -0400

    Update cleanup.sh

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -4,7 +4,8 @@
 rm -f ~/.vimrc
 
 # Remove the line ‘source ∼/.dotfiles/bashrc custom’ in your .bashrc file in your home directory. You can use the ‘sed’ command to accomplish this and replace ‘source ∼/.dotfiles/bashrc custom’ with nothing
-$ sed -i '$d' ~/.bashrc
+# https://lagooko.blogspot.com/2016/01/how-to-delete-lines-using-sed-command.html
+sed -i '$d' ~/.bashrc
 
 # Remove the .TRASH directory inside of your home directory.
 rm -r ~/.TRASH
